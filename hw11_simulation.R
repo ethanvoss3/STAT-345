5227ecd3b3bfbd06bed2fba7e1f200eac56b6bb8
Ethan Voss <EthanVoss@Ethans-MacBook-Air.local>
2018-05-07 17:17:31 -0500

    Hello World!

--- hw11_simulation.R
+++ hw11_simulation.R
@@ -3,3 +3,11 @@ generate_data <- function(n, p) {
   responses <- c(rnorm(n))
   return(list(covariates,responses))
 } 
+
+
+model_select(covariates, responses, cutoff) <- function(covariates, responses, cutoff) {
+  lm(responses ~ covariates)
+  cutoff <- for(1:n) {
+    if(p >cutoff)
+  }
+}
